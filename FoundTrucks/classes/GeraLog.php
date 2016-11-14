ca78c525ad00c494bb03630f8ce481628d75c781
Carlos Bandeira <cbandeira@gmail.com>
2016-11-14 08:39:06 -0200

    Correção do gerador de logs.

--- FoundTrucks/classes/GeraLog.php
+++ FoundTrucks/classes/GeraLog.php
@@ -18,7 +18,7 @@ class GeraLog{
     public function inserirLog($msg){ 
         $msg = "[".date("d-m-Y, H:i:s")."] ".$msg."\n\n"; 
         //$fp = fopen(CAMINHO_RAIZ."admin/logs/error_log_".date("d-m-Y").".txt",'a'); 
-        $fp = fopen("logs/error.log",'a'); 
+        $fp = fopen("classes/logs/error.log",'a'); 
         fwrite($fp,$msg); 
         fclose($fp); 
     } 
