fb0597c34ca95207e9821c9c001307bfb0677c6f
Carlos Bandeira <cbandeira@gmail.com>
2016-11-17 08:19:31 -0200

    Inserção do novo logo na área restrita.

--- FoundTrucks/AreaRestrita/headerRestrita.php
+++ FoundTrucks/AreaRestrita/headerRestrita.php
@@ -6,10 +6,31 @@
 		<nav id="nav">
 			<ul>
 				<li><a href="/">Home</a></li>
-				<li><a href="/parceiros.php">Parceiros</a></li>
-				<li><a href="/sobre.php">Sobre</a></li>				
+				<li><a href="parceiros.php">Parceiros</a></li>
+				<li><a href="sobre.php">Sobre</a></li>
+				<li><a href="#" class="button special" data-toggle="modal" data-target="#login-modal">Login</a></li>
 			</ul>
-		</nav>		
+		</nav>
+
+		<div class="modal fade" id="login-modal" tabindex="-1" role="dialog"
+			aria-labelledby="myModalLabel" aria-hidden="true"
+			style="display: none;">
+			<div class="modal-dialog">
+				<div class="loginmodal-container">
+					<h2 class="text-center" style="color:black !important;padding: 17px 0px;">Entre em sua Conta</h2>
+					<form method="post" action="classes/autenticacao/AutenticacaoUsuario.php">
+						<input type="text" name="teEmail" placeholder="E-mail"> <input
+							type="password" name="teSenha" placeholder="Senha"> <input
+							type="submit" name="login" class="login loginmodal-submit"
+							value="Login">
+					</form>
+
+					<div class="login-help">
+						<a href="#cadastro-modal" data-toggle="modal" data-target="#cadastro-modal" onclick="loginfade();">Cadastrar</a> - <a href="#">Esqueci a senha</a>
+					</div>
+				</div>
+			</div>
+		</div>
 		
 		<div class="modal fade" id="cadastro-modal" tabindex="-1" role="dialog"
 			aria-labelledby="myModalLabel" aria-hidden="true"
